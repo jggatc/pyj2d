d52f53fbced547946bba14080ad803e763a7aca5
James Garnon <jg.gatc@gmail.com>
2014-02-16 21:24:05 -0500

    added surfarray methods

---
+++ numeric.py
@@ -0,0 +1,37 @@
+#PyJ2D - Copyright (C) 2011 James Garnon
+
+#JNumeric module:
+#http://jnumerical.sourceforge.net/
+#JNumeric does not work with Jython2.2.1
+#JNumeric was updated to work with Jython2.5.2:
+#http://search.maven.org/#browse|-1501684728 - jnumeric-0.1.jar
+#Place JNumeric in Java classpath:
+#java -cp jython.jar:jnumeric-0.1.jar:$CLASSPATH org.python.util.jython script.py
+#For a different JNumeric package, modify import statement or use set_numeric_module.
+
+numeric = None
+
+if not numeric:
+    try:
+        from com.github.tbekolay.jnumeric import JNumeric as numeric
+    except ImportError:
+        try:
+            import Numeric as numeric
+        except ImportError:
+            pass
+
+
+def set_numeric_module(module):
+    """
+    Set numeric module if imported a different numeric package.
+    """
+    global numeric
+    numeric = module
+
+
+def get_numeric_module():
+    """
+    Get numeric module. Return None if module not present.
+    """
+    return numeric
+
--- surfarray.py
+++ surfarray.py
@@ -19,6 +19,7 @@ class Surfarray(object):
         Provides image pixel manipulation method.
 
         Module initialization creates pyj2d.surfarray instance.
+        JNumeric module is required as specified in numeric.py.
         """
         self.initialized = False
         self._nonimplemented_methods()
@@ -27,15 +28,59 @@ class Surfarray(object):
         """
         Initialize surfarray module.
         """
-        global Numeric
-        try:
-            import Numeric      #JNumeric
-        except ImportError:
+        global numeric
+        from numeric import numeric
+        if not numeric:
             raise ImportError, "JNumeric module is required."
-        #JNumeric doesn't work on Jython2.2.1
-        #JNumeric updated to work on Jython2.5.2:
-        #https://bitbucket.org/zornslemon/jnumeric-ra/
         self.initialized = True
+
+    def array2d(self, surface):
+        """
+        Return data array of the Surface argument.
+        Array consists of pixel data arranged by [x,y] in integer color format.
+        """
+        if not self.initialized:
+            self._init()
+        data = numeric.zeros((surface.width*surface.height), 'i')
+        data = surface.getRGB(0, 0, surface.width, surface.height, data, 0, surface.width)
+        array = numeric.reshape(data, (surface.width,surface.height))
+        return array
+
+    def array3d(self, surface):
+        """
+        Return data array of the Surface argument.
+        Array consists of pixel data arranged by [x,y] in RGB format.
+        """
+        if not self.initialized:
+            self._init()
+        data = surface.getRGB(0, 0, surface.width, surface.height, None, 0, surface.width)
+        data = numeric.array([(dat>>16 & 0xff, dat>>8 & 0xff, dat & 0xff) for dat in data])
+        array = numeric.reshape(data, (surface.width,surface.height,3))
+        return array
+
+    def array_alpha(self, surface):
+        """
+        Return data array of the Surface argument.
+        Array consists of pixel data arranged by [x,y] of pixel alpha value.
+        """
+        if not self.initialized:
+            self._init()
+        data = surface.getRGB(0, 0, surface.width, surface.height, None, 0, surface.width)
+        data = numeric.array([dat>>24 & 0xff for dat in data], numeric.Int8)
+        array = numeric.reshape(data, (surface.width,surface.height))
+        return array
+
+    def make_surface(self, array):
+        """
+        Generates image pixels from array data.
+        Argument array containing image data.
+        Return Surface generated from array.
+        """
+        if not self.initialized:
+            self._init()
+        surface = Surface((array.shape[0],array.shape[1]))
+        self.blit_array(surface, array)
+        return surface
 
     def blit_array(self, surface, array):
         """
@@ -44,12 +89,17 @@ class Surfarray(object):
         """
         if not self.initialized:
             self._init()
-        w,h = array.shape
-        data = Numeric.reshape(array, (1,w*h))[0]
+        if len(array.shape) == 2:
+            data = numeric.transpose(array, (1,0))
+            data = numeric.ravel(data)
+        else:
+            data = array[:,:,0]*0x10000 | array[:,:,1]*0x100 | array[:,:,2]
+            data = numeric.transpose(data, (1,0))
+            data = numeric.ravel(data)
         if not surface.getColorModel().hasAlpha():
             surface.setRGB(0, 0, surface.width, surface.height, data, 0, surface.width)
         else:
-            surf = Surface((w,h), BufferedImage.TYPE_INT_RGB)
+            surf = Surface((surface.width,surface.height), BufferedImage.TYPE_INT_RGB)
             surf.setRGB(0, 0, surface.width, surface.height, data, 0, surface.width)
             g2d = surface.createGraphics()
             g2d.drawImage(surf, 0, 0, None)
