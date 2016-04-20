185d01bee1a38ec773c2fcaaa706452111bf432d
James Garnon <jg.gatc@gmail.com>
2016-04-20 16:27:09 -0400

    update surface constructor to replace unnecessary method call

--- surface.py
+++ surface.py
@@ -55,7 +55,10 @@ class Surface(BufferedImage):
                     BufferedImage.__init__(self, width, height, BufferedImage.TYPE_INT_RGB)
             except IndexError:
                 BufferedImage.__init__(self, width, height, BufferedImage.TYPE_INT_ARGB)
-                self.fill((0,0,0))
+                graphics2D = BufferedImage.createGraphics(self)
+                graphics2D.setColor(Color(0,0,0))
+                graphics2D.fillRect(0, 0, width, height)
+                graphics2D.dispose()
         except TypeError:
             try:
                 cm = arg[0].getColorModel()
