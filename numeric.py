60dd30c46cdfcaf3906a2e446d3028f89d206bf6
James Garnon <jg.gatc@gmail.com>
2021-11-07 13:23:54 -0500

    update documentation

--- numeric.py
+++ numeric.py
@@ -4,7 +4,7 @@
 #JNumeric module:
 #http://jnumerical.sourceforge.net/
 #JNumeric does not work with Jython2.2.1
-#JNumeric was updated to work with Jython2.5.2:
+#JNumeric was updated to work with Jython2.5.3:
 #http://search.maven.org/#browse|-1501684728 - jnumeric-0.1.jar
 #Place JNumeric in Java classpath:
 #java -cp jython.jar:jnumeric-0.1.jar:$CLASSPATH org.python.util.jython script.py
