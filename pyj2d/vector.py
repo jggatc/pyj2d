b349f5cae22e5e5520d66f1b4b9c68b437c7ca19
James Garnon <jg.gatc@gmail.com>
2023-10-25 19:27:37 -0400

    add vector copy method

--- pyj2d/vector.py
+++ pyj2d/vector.py
@@ -369,6 +369,12 @@ class Vector2(object):
         self._y = round(r * sin(phi), 6)
         return None
 
+    def copy(self):
+        """
+        Return copy of vector.
+        """
+        return Vector2(self._x, self._y)
+
     def update(self, *args, **kwargs):
         """
         Update vector.
